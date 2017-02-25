32608f869ec155802bc58f5f10be0de3cff0d95a
fungjeff <jeffrey.fung@berkeley.edu>
2017-02-24 18:22:31 -0800

    Add files via upload

--- src/orbital_advection/orbital_advection.cu
+++ src/orbital_advection/orbital_advection.cu
@@ -9,51 +9,62 @@
 #include "cuSafe.cu"
 
 //================================================================================
-__global__ void Index_Shift(hydr_ring *rings, sdp dt)
+__global__ void Index_Shift(hydr_ring *rings, SymDisk *val, sdp iblk, sdp dt, sdp FrRot)
 {
   int n = blockIdx.x + gridDim.x * blockIdx.y;
-  sdp jp;
+  sdp rot_v, jp;
   sdp widthy;
 
   widthy = rings[n].xc*rings[n].dy[0] / dt;
+
+  #if FARGO_flag == 1
+  int idx = rings[n].i + iblk * rings[n].k;
+  rot_v = val[idx].v - FrRot*rings[n].xc;
+  #elif FARGO_flag == 2
+  rot_v = (1.0 - FrRot)*rings[n].xc;
+  #else
+  rot_v = 0.0;
+  #endif
+
   jp = (rings[n].rot_v/widthy);
   if (jp>=0.0) 
   {
     rings[n].inc_j = (int)(jp+0.5);
-    rings[n].res_v = rings[n].rot_v - (floor(jp+0.5)*widthy);
+    rings[n].res_v = floor(jp+0.5)*widthy;
   }
   else
   {
     rings[n].inc_j = (int)(jp-0.5);
-    rings[n].res_v = rings[n].rot_v - (floor(jp+0.5)*widthy);
+    rings[n].res_v = floor(jp+0.5)*widthy;
   }
-
-  //if (n==0) printf("%i : %f %i %f %f\n", n, dt, rings[n].inc_j, rings[n].res_v, rings[n].rot_v);
-  //if (n==0) printf("%f %i %i %f\n", jp, rings[n].rot_j, rings[n].inc_j, rings[n].rot_v*cpow(rings[n].xc,0.5));
+  rings[n].rot_v = rot_v;
 
   return;
 }
 
-void Index_Shift(GPU_plan *set, sdp dt)
+void Index_Shift(GPU_plan *set, sdp dt, sdp FrRot)
 {
   for(int n=0; n<nDev; n++)
   {
     CudaSafeCall( cudaSetDevice(set[n].id) );
 
-    Index_Shift<<< set[n].sy_grid , 1 , 0 , set[n].stream >>> (set[n].rings, dt);
+    Index_Shift<<< set[n].sy_grid , 1 , 0 , set[n].stream >>> (set[n].rings, set[n].val, set[n].iblk, dt, FrRot);
     CudaCheckError();
   }
-
-  for (int i=0; i<nDev; i++)  CudaSafeCall( cudaStreamSynchronize(set[i].stream) );
 /*
-  for(int i=0; i<imax; i++)
+  for (int n=0; n<nDev; n++)
   {
-    CudaSafeCall( cudaMemcpy( set[0].h_rings, set[0].rings, set[0].memsize, cudaMemcpyDeviceToHost ) );
-    printf("%i : %f %i %f(%f) %f\n", i, dt, set[0].h_rings[i].inc_j, set[0].h_rings[i].res_v, set[0].h_rings[i].res_v*dt/set[0].h_rings[i].dy[0]/set[0].h_rings[i].xc, set[0].h_rings[i].rot_v);
-    printf("%i : %e\n", i, set[0].h_rings[i].v[0]*pow(set[0].h_rings[i].xc,0.5)-1.0);
+    CudaSafeCall( cudaMemcpy( set[n].h_rings, set[n].rings, set[n].memsize, cudaMemcpyDeviceToHost ) );
+    for (int i=0; i<set[n].iblk; i++)
+    {
+      printf(" %f %f %f %i %i \n", set[n].h_rings[i].xc, set[n].h_rings[i].rot_v*dt/set[n].h_rings[i].dy[0]/set[n].h_rings[i].xc, (set[n].h_rings[i].res_v)*dt/set[n].h_rings[i].dy[0]/set[n].h_rings[i].xc, set[n].h_rings[i].inc_j, set[n].h_rings[i].rot_j);
+    }
   }
-    wait_f_r();
+  cout << FrRot << endl;
+  wait_f_r();
 */
+  syncstreams(set);
+
   return;
 }
 
